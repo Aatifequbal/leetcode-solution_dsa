8d2c54f83c6329c9860174d2bb62781d5b99bfa2
Aatif Equbal <atifequbal1916@gmail.com>
2026-07-26 03:22:01 +0530

    Time: 1 ms (99.97%), Space: 42.5 MB (80.32%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,31 @@
+// class Solution {
+//     public int reverse(int x) {
+//         int revNum = 0;
+//         while(x != 0){
+//              int digit =  x % 10;
+//             revNum = revNum * 10 + digit;
+//             x = x/10;
+
+//         } 
+//         return revNum;
+        
+//     }
+    
+// }
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return (int) rev;
+    }
+}
