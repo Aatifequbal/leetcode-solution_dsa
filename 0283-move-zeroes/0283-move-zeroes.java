0ff0a7da46239e5dde99115fa7867301f4fa1a3c
Aatif Equbal <atifequbal1916@gmail.com>
2026-08-20 19:33:56 +0530

    Time: 2 ms (92%), Space: 47.8 MB (38.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,6 +2,8 @@ class Solution {
     public void moveZeroes(int[] nums) {
        int j=0;
        int n= nums.length;
+       //TC o(n)
+       //SC 0(1)
 
        for(int i=0; i<n; i++){
             if(nums[i] != 0){
